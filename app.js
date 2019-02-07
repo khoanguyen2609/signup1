dbc09f3de67493e9fa9d1884fd6d5a028225482e
khoanguyen2609 <39813628+khoanguyen2609@users.noreply.github.com>
2019-02-07 15:41:18 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//call express
+var app = express();
+
+// call body parser
+app.use(bodyParser.urlencoded({extend: false}));
+
+//call static
+
+//call ejs
+app.set("views", __dirname + "/apps/views");
+app.set("view engine" , 'ejs');
+
+app.get("/", function( req , res){
+  res.render('signup.ejs');
+});
+
+app.post("/", function( req , res) {
+  res.render("ok.ejs", {data : req.body});
+})
+
+app.listen (3000, function(){
+  console.log('port 3000');
+})
